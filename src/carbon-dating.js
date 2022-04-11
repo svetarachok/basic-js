ddfb97ce04e163193cd4c1d64c3a93cad11db75d
Sveta Rachok <sveta.r1212@gmail.com>
2022-04-11 21:04:39 +0300

    feat: add solutions - count-cats, Carbon dating, Dream team, What season?, Tower of Hanoi

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,14 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  let pastActivity = Number(sampleActivity)
+  if (!sampleActivity || typeof sampleActivity !== 'string' || !pastActivity || pastActivity > MODERN_ACTIVITY || pastActivity <= 0) {
+    return false
+  }
+  let k = 0.693/HALF_LIFE_PERIOD;
+  let result = Math.log(pastActivity/MODERN_ACTIVITY)/k;
+  return Math.abs(Math.floor(result))
 }
 
 module.exports = {
--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  let firstLetters = [];
+  if (!Array.isArray(members)) {
+    return false
+  }
+
+  firstLetters = members.filter(item => typeof item === 'string')
+  .map(item => item.trim().charAt(0).toUpperCase())
+
+  
+  return firstLetters.sort((a,b) => a >= b ? 1 : -1).join('')
 }
 
 module.exports = {
