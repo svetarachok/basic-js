177a5fd141851619d3649d2ff4ad9d7b552ba91a
Sveta Rachok <sveta.r1212@gmail.com>
2022-04-30 00:32:38 +0300

    feat: add solution - Extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,45 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function repeater(str, options) {
+
+  let innerObj = {
+        repeatTimes: 1,
+        separator: "+",
+        addition: '',
+        additionRepeatTimes: 1,
+        additionSeparator: '|',
+
+    }
+
+    for (let key in options) {
+        innerObj[key] = options[key];
+      }
+
+
+  if(typeof str !== 'string' && str) {
+      str = `${str}`
+  }
+  if (typeof innerObj.addition !== 'string' && innerObj.addition) {
+    innerObj.addition = `${innerObj.addition}`
+  }
+  console.log(innerObj.addition)
+  // if (innerObj.additionRepeatTimes <= 1 && innerObj.additionSeparator === undefined) {
+  //   innerObj.additionSeparator = "|"
+  // }
+
+ let arr = [];
+ let addString = (innerObj.addition + innerObj.additionSeparator).repeat(innerObj.additionRepeatTimes);
+ let finAddString = addString.slice(0, addString.length - innerObj.additionSeparator.length)
+
+  for (let i = 0; i< innerObj.repeatTimes; i++) {
+     if(i === innerObj.repeatTimes-1){
+          arr.push(str + finAddString)
+      } else {
+         arr.push(str + finAddString + innerObj.separator) 
+      }
+  }
+  return arr.join('')
 }
 
 module.exports = {
